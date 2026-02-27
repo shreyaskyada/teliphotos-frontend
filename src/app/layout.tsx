b6867a929c496240d962abffacc10fbb33331218
shreyas <kyadashreyas@gmail.com>
2026-02-27 10:30:23 +0530

    added google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata, Viewport } from "next";
 import { Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 export const viewport: Viewport = {
@@ -27,6 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`dark scrollbar-thin ${outfit.variable}`}>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1375243567926496"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={`font-sans antialiased bg-background`}
       >
